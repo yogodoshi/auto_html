15668d0efc043491a122615c9a6b34ace6e5cacf
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-03-18 23:03:13 -0300

    The youtube filter now includes youtube links without 'http://'

--- test/unit/filters/youtube_test.rb
+++ test/unit/filters/youtube_test.rb
@@ -16,7 +16,7 @@ class YouTubeTest < Test::Unit::TestCase
     result = auto_html('http://www.youtube.com/watch?v=BwNrmYRiX_o&feature=related') { youtube }
     assert_equal '<iframe width="420" height="315" src="//www.youtube.com/embed/BwNrmYRiX_o" frameborder="0" allowfullscreen></iframe>', result
   end
-  
+
   def test_transform3
     result = auto_html('foo http://www.youtube.com/watch?v=fT1ahr81HLw bar') { youtube }
     assert_equal 'foo <iframe width="420" height="315" src="//www.youtube.com/embed/fT1ahr81HLw" frameborder="0" allowfullscreen></iframe> bar', result
@@ -36,14 +36,24 @@ class YouTubeTest < Test::Unit::TestCase
     result = auto_html('http://www.youtu.be/BwNrmYRiX_o') { youtube }
     assert_equal '<iframe width="420" height="315" src="//www.youtube.com/embed/BwNrmYRiX_o" frameborder="0" allowfullscreen></iframe>', result
   end
-  
+
   def test_transform_https
     result = auto_html("https://www.youtube.com/watch?v=t7NdBIA4zJg") { youtube }
     assert_equal '<iframe width="420" height="315" src="//www.youtube.com/embed/t7NdBIA4zJg" frameborder="0" allowfullscreen></iframe>', result
   end
-  
+
   def test_short_with_params
     result = auto_html("http://youtu.be/t7NdBIA4zJg?t=1s&hd=1") { youtube }
     assert_equal '<iframe width="420" height="315" src="//www.youtube.com/embed/t7NdBIA4zJg" frameborder="0" allowfullscreen></iframe>', result
+  end
+
+  def test_transform_url_without_http
+    result = auto_html('http://www.youtube.com/watch?v=BwNrmYRiX_o') { youtube }
+    assert_equal '<iframe width="420" height="315" src="//www.youtube.com/embed/BwNrmYRiX_o" frameborder="0" allowfullscreen></iframe>', result
+  end
+
+  def test_transform_url_without_http_and_www
+    result = auto_html('youtube.com/watch?v=BwNrmYRiX_o') { youtube }
+    assert_equal '<iframe width="420" height="315" src="//www.youtube.com/embed/BwNrmYRiX_o" frameborder="0" allowfullscreen></iframe>', result
   end
 end
