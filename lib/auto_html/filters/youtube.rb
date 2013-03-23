ee29400cc739c24eedd5de467d37001e619ad1d6
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-03-23 17:58:06 -0300

    Making youtube filter work for URLs with lots of parameters

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,7 +1,7 @@
 AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil) do |text, options|
-  regex = /(https?:\/\/)?(www.)?(m\.youtube\.com\/\#\/watch\?(feature=plcp&)?v=|youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
+  regex = /(https?:\/\/)?(www\.|m\.)?(youtube\.com\/(\#\/)?watch\?(.*)?v=|youtube\.com\/watch\?v=|youtu\.be\/)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
   text.gsub(regex) do
-    youtube_id = $5
+    youtube_id = $6
     width = options[:width]
     height = options[:height]
     frameborder = options[:frameborder]
